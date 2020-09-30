fa14ea4c9d2d3dfb4712290d162bee4aadc5b10a
Quarter <stas.batalenkov@mail.ru>
2020-09-30 13:12:26 +0300

    Initial commit

---
+++ src/main/java/ru/quarter/zc/ZombieCarriers.java
@@ -0,0 +1,29 @@
+package ru.quarter.zc;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ZombieCarriers.MODID, name = ZombieCarriers.MODNAME, version = ZombieCarriers.VERSION)
+public class ZombieCarriers {
+
+    public static final String MODID = "zc";
+    public static final String MODNAME = "ZombieCarriers";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/ru/quarter/zc/repack/gloomyfolken/hooklib/asm/HookPriority.java
@@ -0,0 +1,11 @@
+package ru.quarter.zc.repack.gloomyfolken.hooklib.asm;
+
+public enum HookPriority {
+
+    HIGHEST, // Вызывается первым
+    HIGH,
+    NORMAL,
+    LOW,
+    LOWEST // Вызывается последним
+
+}
